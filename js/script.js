0b3ed7fafa2d25e9c110964328b8b7fb62673eb3
Glorytefe <glorytefe@gmail.com>
2021-04-09 02:40:38 +0100

    upadated portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nav = document.getElementById('navs')
+let lis = document.getElementById('uls');
+let checkbx = document.getElementById("checkbx")
+
+
+
+lis.addEventListener('click', ()=>{
+     if (checkbx.checked = true && nav.style.display == "block" ){
+        checkbx.checked = false
+     } else{
+         lis.classList.add('ulin')
+     }
+    })
